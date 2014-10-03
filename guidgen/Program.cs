4fcaca4ef352293355892cddf6f4e4e14071a82a
Florian Mücke <fmuecke@baramundi.de>
2014-10-04 00:33:20 +0200

    added guidgen: will output and copy new guid to clipboard

---
+++ guidgen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guidgen
+{
+    internal class Program
+    {
+        [STAThreadAttribute]
+        private static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            string s = guid.ToString().ToUpperInvariant();
+            Console.Write(s);
+            Clipboard.SetText(s);
+        }
+    }
+}
